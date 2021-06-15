0898cc1b8ae5b68a039feabdf5f9e685071bf096
Shasha Yan <shashayan@gmail.com>
2021-06-14 23:21:14 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,12 @@ class SongsController < ApplicationController
   end
 
   def new
+    @preference = Preference.first 
+    if @preference.allow_create_artists
     @song = Song.new
+    else  
+      redirect_to songs_path
+    end
   end
 
   def create
